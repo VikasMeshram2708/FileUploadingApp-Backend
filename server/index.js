46025e0a707d6daeb8e4e288c3e34a99e3869d05
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-03-10 15:41:04 +0530

    :tada: configured the html file with the backend.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
+const path = require("path");
 
 const app = express();
 
 // middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// routing for static files
+app.use("/", express.static(path.join(__dirname, "views")));
 
 // routes
 app.get("/", (req, res) => {
